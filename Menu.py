e270db445533dc049aac1454b1ed73a23cdfc07f
Ouertani_Mabrouk <ouertani.mabrouk.08@itisdalmine.edu.it>
2024-12-17 09:58:21 +0100

    Create Menu.py

---
+++ Menu.py
@@ -0,0 +1,14 @@
+while True:
+    
+    print("benvenuto nella nostra calcolatrice") 
+    print("inserisci l'operazione che vuoi effettuare: ") 
+    scelta =int(input("1)sottrazione\n2)addizione\n") )
+    if scelta ==0 : 
+        break 
+    elif scelta==1: 
+        sottrazione() 
+
+    elif scelta==2: 
+          somma()  
+    else: 
+        print("scelta non corretta !!")
