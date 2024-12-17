bc0abe0a5fa62f0e4ddeed21add8e8231d609c14
Ouertani_Mabrouk <ouertani.mabrouk.08@itisdalmine.edu.it>
2024-12-17 09:45:10 +0100

    Update calcolatrice.py

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,4 +1,8 @@
-scelta=-1 
+def somma():  
+    n_1=int(input("inserisci il primo numero !")) 
+        n_2=int(input("inserisci il secondo numero !")) 
+        risultato=n_1+n_2
+        print(f"{n_1}+{n_2}={risultato}")
 while True:
     
     print("benvenuto nella nostra calcolatrice") 
@@ -13,9 +17,6 @@ while True:
         print(f"{n_1}-{n_2}={risultato}")
 
     elif scelta==2: 
-        n_1=int(input("inserisci il primo numero !")) 
-        n_2=int(input("inserisci il secondo numero !")) 
-        risultato=n_1+n_2
-        print(f"{n_1}+{n_2}={risultato}")    
+          somma()  
     else: 
         print("scelta non corretta !!")
