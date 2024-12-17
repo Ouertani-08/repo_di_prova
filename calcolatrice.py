05d9bec341f4e7270a2f59e68bf65b6e04233f88
MABROUK OUERTANI <ouertani.mabrouk.08@itisdalmine.edu.it>
2024-12-17 09:37:38 +0100

    implementazione base della calcolatrice

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,17 +1,20 @@
-print("benvenuto nella nostra calcolatrice") 
-print("inserisci l'operazione che vuoi effettuare: ") 
-scelta =int(input("1)sottrazione\n2)addizione\n") )
+scelta=-1 
+while (scelta!=0):
+    
+    print("benvenuto nella nostra calcolatrice") 
+    print("inserisci l'operazione che vuoi effettuare: ") 
+    scelta =int(input("1)sottrazione\n2)addizione\n") )
 
-if scelta==1: 
-    n_1=int(input("inserisci il primo numero !")) 
-    n_2=int(input("inserisci il secondo numero !")) 
-    risultato=n_1-n_2
-    print(f"{n_1}-{n_2}={risultato}")
+    if scelta==1: 
+        n_1=int(input("inserisci il primo numero !")) 
+        n_2=int(input("inserisci il secondo numero !")) 
+        risultato=n_1-n_2
+        print(f"{n_1}-{n_2}={risultato}")
 
-elif scelta==2: 
-    n_1=int(input("inserisci il primo numero !")) 
-    n_2=int(input("inserisci il secondo numero !")) 
-    risultato=n_1+n_2
-    print(f"{n_1}+{n_2}={risultato}")    
-else: 
-    print("scelta non corretta !!")
+    elif scelta==2: 
+        n_1=int(input("inserisci il primo numero !")) 
+        n_2=int(input("inserisci il secondo numero !")) 
+        risultato=n_1+n_2
+        print(f"{n_1}+{n_2}={risultato}")    
+    else: 
+        print("scelta non corretta !!")
